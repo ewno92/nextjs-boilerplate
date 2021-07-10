115580401263eec3db4f0a9d9478a991093bf54a
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-07-10 02:37:35 -0700

    db connected

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,29 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 // app
 const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connected"));
 
 // middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 // cors
-app.use(cors());
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 // routes
 app.get("/api", (req, res) => {

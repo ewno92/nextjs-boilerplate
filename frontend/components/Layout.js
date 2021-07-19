56f2124d4fbacfd4aebfe48d9cd2d7a2ce8c3956
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-07-19 11:13:01 -0700

    navbar added

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,9 +1,8 @@
-import React from "react";
-
+import Header from "./Header";
 const Layout = ({ children }) => {
   return (
     <div>
-      <p>header</p>
+      <Header />
       {children}
       <p>footer</p>
     </div>
